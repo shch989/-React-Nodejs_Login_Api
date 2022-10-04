1cf0ae1096032a1f57003d3e6254dbc0c0b88e66
shch989 <tankpop11@gmail.com>
2022-10-04 21:25:29 +0900

    Auth 기능 생성

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
 
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello Express!!!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 
   // 그것들을 데이터 베이스에 넣어준다.
@@ -38,7 +39,7 @@ app.post('/register', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   // 요청된 이메일을 데이터베이스에서 있는지 확인
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -68,6 +69,20 @@ app.post('/login', (req, res) => {
       .json({ loginSuccess: true, userId: user._id })
       })
     })
+  })
+})
+
+app.get('/api/users/auth',auth ,(req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말
+  req.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth : true,
+    email : req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
   })
 })
 
