cd0b625cd3a2ae877920a2c4c0503ff934796db0
shch989 <tankpop11@gmail.com>
2022-10-04 20:43:14 +0900

    web 토큰을 생성하여 쿠키에 저장

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
 
@@ -48,8 +49,32 @@ userSchema.pre("save", function( next ) {
         next()
       })
     });
+  } else {
+    next()
   }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  // platinPassword와 DB의 암호화된 비밀번호를 비교
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+      cb(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+  var user = this
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return cb(err)
+    cb(null, user)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
